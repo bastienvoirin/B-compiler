c1ff2d6006a5a02da9036a09302d4f92e6a31ecc
Bastien Voirin <bastien.voirin.pro@gmail.com>
2020-04-12 17:04:56 +0200

    Structure change

--- lexer.c
+++ lexer.c
@@ -13,14 +13,7 @@ main() {
     tk = getvec(2047); // 1024 tokens
     names = getvec(255); // 32 names
     while (src[i]) {
-        if (src[i] && src[i] <= " ") {
-            l++;
-            k = 0;
-            while (src[++i] && src[i] <= " ");
-        }
         switch (src[i]) {
-            case NULL:
-                break;
             case "0" :: "9":
             case "a" :: "z":
             case "A" :: "Z":
@@ -28,91 +21,89 @@ main() {
             case ".":
                 if (k < 8) names[l + k++] = src[i++];
                 break;
-            case "(":
-            case "[":
-            case "{":
-            case ")":
-            case "]":
-            case "}":
-                l++;
-                k = 0;
-                tk[j++] = str[i];
-                tk[j++] = NULL;
-                break;
-            case "'":
-            case '"':
-            case "`":
-                l++;
-                k = 0;
-                c = src[i];
-                i++;
-                while (src[i] && (src[i] != c || esc(src, i))) {
-                    i++;
+            default:
+                if (k) {
+                    l++;
+                    k = 0;
                 }
-                break;
-            case "=":
-            case "<":
-            case ">":
-                l++;
-                k = 0;
-                tk[j++] = src[i];
-                if (src[++i] == src[i] || src[i] == "=") {
-                    tk[j++] = src[i];
-                } else {
-                    i--;
-                    tk[j++] = NULL;
-                }
-                break;
-            case "|":
-            case "&":
-            case "+":
-            case "-":
-                l++;
-                k = 0;
-                tk[j++] = src[i];
-                i++;
-                if (src[i] == src[i-1] || src[i] == "=") {
-                    tk[j++] = src[i];
-                } else {
-                    i--;
-                    tk[j++] = NULL;
-                }
-                break;
-            case "**":
-            case "/":
-            case "!":
-            case "~":
-            case "^":
-            case "%":
-                l++;
-                k = 0;
-                tk[j++] = src[i];
-                tk[j++] = NULL;
-                break;
-            case "#":
-                l++;
-                k = 0;
-                tk[j++] = "#";
-                switch (src[++i]) {
-                    case "#":
+                switch (src[i]) {
+                    case ";":
+                    case "*n":
+                        tk[j++] = ";";
+                        tk[j++] = NULL;
+                    case NULL:
+                        break;
+                    case "(":
+                    case "[":
+                    case "{":
+                    case ")":
+                    case "]":
+                    case "}":
+                        tk[j++] = str[i];
+                        tk[j++] = NULL;
+                        break;
+                    case "'":
+                    case '"':
+                    case "`":
+                        c = src[i];
+                        i++;
+                        while (src[i] && (src[i] != c || esc(src, i))) {
+                            i++;
+                        }
+                        break;
+                    case "=":
+                    case "<":
+                    case ">":
+                        tk[j++] = src[i];
+                        if (src[++i] == src[i] || src[i] == "=") {
+                            tk[j++] = src[i];
+                        } else {
+                            i--;
+                            tk[j++] = NULL;
+                        }
+                        break;
+                    case "|":
+                    case "&":
                     case "+":
                     case "-":
+                        tk[j++] = src[i];
+                        i++;
+                        if (src[i] == src[i-1] || src[i] == "=") {
+                            tk[j++] = src[i];
+                        } else {
+                            i--;
+                            tk[j++] = NULL;
+                        }
+                        break;
                     case "**":
                     case "/":
+                    case "!":
+                    case "~":
+                    case "^":
+                    case "%":
                         tk[j++] = src[i];
+                        tk[j++] = NULL;
+                        break;
+                    case "#":
+                        tk[j++] = "#";
+                        switch (src[++i]) {
+                            case "#":
+                            case "+":
+                            case "-":
+                            case "**":
+                            case "/":
+                                tk[j++] = src[i];
+                                break;
+                            default:
+                                tk[j++] = NULL;
+                                i--;
+                                break;
+                        }
                         break;
                     default:
-                        tk[j++] = NULL;
-                        i--;
                         break;
                 }
-                break;
-            case ";":
-            default:
-                l++;
-                k = 0;
-                break;
         }
-        i++;
+        while (src[++i] && src[i] <= " ");
     }
 }
